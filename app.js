604f2b3d0e23fc3ac9275e75eef1acd8901b0ad9
adnanmouloud <adnanmouloud@gmail.com>
2020-03-22 13:02:41 +0100

    SDI-NodeJS-8.2 - Acceso a datos de Mongo desde Node.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let express = require('express');
 let app = express();
 
 let swig = require('swig');
+let mongo = require('mongodb');
 
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -12,11 +13,12 @@ app.use(express.static('public'));
 
 // Variables
 app.set('port', 8081);
+app.set('db','mongodb://admin:sdi@tiendamusica-shard-00-00-dt0jd.mongodb.net:27017,tiendamusica-shard-00-01-dt0jd.mongodb.net:27017,tiendamusica-shard-00-02-dt0jd.mongodb.net:27017/test?ssl=true&replicaSet=tiendamusica-shard-0&authSource=admin&retryWrites=true&w=majority');
 
 //Rutas/controladores por lógica
-require("./routes/rusuarios")(app, swig);
-require("./routes/rcanciones")(app, swig);
-require("./routes/rautores")(app, swig);
+require("./routes/rusuarios")(app, swig, mongo);
+require("./routes/rcanciones")(app, swig, mongo);
+require("./routes/rautores")(app, swig, mongo);
 
 //lanzar el servidor
 app.listen(app.get('port'), function () {
